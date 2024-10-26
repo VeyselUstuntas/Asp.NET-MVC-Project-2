9e2cf7d20049a195ad66ae2d1d67855a4efd790e
veyselustuntas <veyselustuntas@hotmail.com>
2024-10-26 15:18:48 +0300

    connection string and dotnet ef-core configuration completed

--- Models/RepositoryContext.cs
+++ Models/RepositoryContext.cs
@@ -5,5 +5,10 @@ namespace StoreApp.Models
     public class RepositoryContext : DbContext
     {
         public DbSet<Product> Products {set;get;}
+
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+            
+        }
     }
 }   
